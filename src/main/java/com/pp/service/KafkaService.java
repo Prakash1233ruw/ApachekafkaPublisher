c5d709d02dc7ed50be821846a5442ce5d8c5855a
prakash <pr0018908@gmail.com>
2024-07-05 13:08:37 +0530

    publisher added

---
+++ src/main/java/com/pp/service/KafkaService.java
@@ -0,0 +1,37 @@
+package com.pp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.pp.model.Customer;
+import com.pp.util.AppConstants;
+
+@Service
+public class KafkaService {
+
+	@Autowired
+	private KafkaTemplate<String, Customer> kafkaTemplate;
+	
+	
+	public String addMsg(Customer cx) {
+
+		System.out.println(cx + " is in service");
+		kafkaTemplate.send(AppConstants.TOPIC_NAME, cx);
+         System.out.println("Able to send data");
+		return "Msg Published To Kafka Topic";
+	}
+	
+	public String add(List<Customer> customers) {
+
+		if (!customers.isEmpty()) {
+			for (Customer c : customers) {
+				kafkaTemplate.send(AppConstants.TOPIC_NAME, c);
+				System.out.println("************Msg published to Kafka topic***************");
+			}
+		}
+		return "Customer Records Added To Kafka  Successfully";
+}
+}
